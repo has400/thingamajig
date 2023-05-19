3fb7459fb9884a906745a5b043e1af39f49dcae9
Harrison Smith <harrison.smith@slalom.com>
2023-05-19 14:40:49 +1000

    updates to remove next js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 
 import { api } from "npm/utils/api";
@@ -5,7 +6,11 @@ import { api } from "npm/utils/api";
 import "npm/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+  <ClerkProvider {...pageProps}>
+    <Component {...pageProps} />;
+  </ClerkProvider>
+  )
 };
 
 export default api.withTRPC(MyApp);
